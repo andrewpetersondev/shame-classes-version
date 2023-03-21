d6b9ca5f44a27d1d6237fc51df129cc308839f9d
Andrew Peterson <andrewpetersondev@gmail.com>
2023-03-21 17:22:27 -0500

    added try-catch to mongoose connection

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-const uri = process.env.MONGODB_URI || "mongodb://localhost/shame_db";
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, {
   useFindAndModify: false,
@@ -29,14 +29,24 @@ mongoose.connect(uri, {
   useUnifiedTopology: true
 });
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("mongoDB database connection established successfully");
-}).then(() => {
-  console.log('connected to MongoDB');
-}).catch((error) => {
-  console.error("error connecting to mongoDB: ", error.message)
-})
+try {
+  const connection = mongoose.connection;
+  // console.log(connection);
+  connection.once("open", () => {
+    console.log("mongoDB database connection established successfully");
+  })
+} catch (err) {
+  console.error(err)
+}
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("mongoDB database connection established successfully");
+// }).then(() => {
+//   console.log('connected to MongoDB');
+// }).catch((error) => {
+//   console.error("error connecting to mongoDB: ", error.message)
+// })
 
 // Start the API server
 app.listen(PORT, function () {
