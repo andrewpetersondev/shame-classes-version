43c1c1999932fe6a5a091feeb8b951e3ff931050
Andrew Peterson <andrewpetersondev@gmail.com>
2023-03-21 19:15:42 -0500

    added try-catch promise to api for  homepage

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,76 +2,82 @@ import axios from "axios";
 export default {
 
     // API Server requests
-    registerUser: function(userData) {
+    registerUser: function (userData) {
         return axios.post('/api/user', userData);
     },
 
-    authenticateUser: function(loginData) {
+    authenticateUser: function (loginData) {
         return axios.post('/api/user/login', loginData);
     },
 
-    getUserDetails: function(userId) {
+    getUserDetails: function (userId) {
         // console.log('API request - getUserDetails');
         return axios.get('/api/user/' + userId);
     },
 
-    addCategory: function(categoryData) {
+    addCategory: function (categoryData) {
         // console.log('API request - addCategory');
         return axios.post('/api/category', categoryData);
     },
 
-    getCategories: function() {
+    getCategories: function () {
         // console.log('API request - getCategories');
-        return axios.get('/api/category');
+        return axios.get('/api/category').then(function (response) {
+            console.log("getCategories response.data= ", response.data)
+            return response.data;
+        }).catch(function (error) {
+            console.error("error getting categories: ", error);
+            throw error;
+        });
     },
-    getCategoryMatch: function(categoryId) {
+    getCategoryMatch: function (categoryId) {
         // console.log('API request - getCategoryMatch');
         return axios.get('/api/category/' + categoryId);
     },
 
-    addGoal: function(goalData) {
+    addGoal: function (goalData) {
         // console.log('API request - addGoal');
         return axios.post('/api/goal', goalData);
     },
-    getGoalsInCategory: function(categoryId) {
+    getGoalsInCategory: function (categoryId) {
         // console.log('API request - getGoalsInCategory');
         return axios.get('/api/goal/cat/' + categoryId);
     },
-    getGoalMatch: function(goalId) {
+    getGoalMatch: function (goalId) {
         // console.log('API request - getGoalMatch');
         return axios.get('/api/goal/' + goalId);
     },
 
-    addTask: function(taskData) {
+    addTask: function (taskData) {
         // console.log('API request - addTask');
         return axios.post('/api/task', taskData);
     },
-    getTasksInGoal: function(goalId) {
+    getTasksInGoal: function (goalId) {
         // console.log('API request - getTasksInGoal');
         return axios.get('/api/task/' + goalId);
     },
 
-    addUserGoal: function(userGoalData) {
+    addUserGoal: function (userGoalData) {
         // console.log('API request - addUserGoal');
         return axios.post('/api/usergoal', userGoalData);
     },
-    addTimeline: function(timelineData) {
+    addTimeline: function (timelineData) {
         // console.log('API request - add timeline entry');
         return axios.post('/api/tasktimeline', timelineData);
     },
-    deleteTimeline: function(timelineId) {
+    deleteTimeline: function (timelineId) {
         // console.log('API request - delete timeline entry');
         return axios.delete('/api/tasktimeline/' + timelineId);
     },
-    email: function(emailAddr) {
+    email: function (emailAddr) {
         // console.log('API request - send an email');
         return axios.post('/api/email/', emailAddr);
     },
-    getTaskTimeline: function(taskId, userGoalId) {
+    getTaskTimeline: function (taskId, userGoalId) {
         // console.log('API request - get task timeline entries for a task/usergoal');
         return axios.get('/api/tasktimeline/task/' + taskId + '/' + userGoalId);
     },
-    updateGoalPercent: function(userGoalId, goalPercent) {
+    updateGoalPercent: function (userGoalId, goalPercent) {
         // console.log('API request - update goalPercenta for userGoalId ' + userGoalId);
         return axios.put('/api/usergoal//goalpercent/' + userGoalId + '/' + goalPercent);
     }
